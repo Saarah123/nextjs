d0d23a119c5919884070acf3c1ea7696ccac1731
Saarah26 <saarahkashyap@gmail.com>
2022-07-22 13:19:06 +0530

    file routing

---
+++ weather/pages/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Error = () => {
+  return (
+    <div>
+         <div className='error'>
+        <h3 style={{marginTop:"10%", marginLeft:"40%"}}>Sorry Can't find the Location</h3>
+
+      
+    </div>
+    </div>
+  )
+}
